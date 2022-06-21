f5af5be5a7f2b1effd447d4da1559a46bb3b1ef2
Tyler Roll <tylerroll@Tylers-MacBook-Pro.local>
2022-06-21 12:17:57 -0600

    deploy to ganache

---
+++ contracts/ToDoList.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.5.16;
+
+contract ToDoList{
+    //state variables are written to blockchain cost more gas to use
+    uint public taskCount = 0;
+}
